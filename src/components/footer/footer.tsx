a370bcae6d7f36d27cb7f9ecdef4a47d20bdfba5
Chinh Khong <khongchinh757@gmail.com>
2025-04-01 14:13:58 +0700

    use constant format code

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,124 +1,104 @@
+import { IMAGE_URL } from "@/public";
 import React from "react";
-export const IMAGE_URL = {
-  FACEBOOK: "./img/facebook.jpg",
-  TWITTER: "./img/twitter.jpg",
-  INSTAGRAM: "./img/instagram.jpg",
-};
-// src/components/footer.tsx
+
 const Footer = () => {
+
+  const FOOTER_SECTIONS = [
+    {
+      title: "Support",
+      items: [
+        "Help Center",
+        "AirCover",
+        "Anti-discrimination",
+        "Disability support",
+        "Cancellation options",
+        "Report neighborhood concern",
+      ],
+    },
+    {
+      title: "Hosting",
+      items: [
+        "Airbnb your home",
+        "AirCover for Hosts",
+        "Hosting resources",
+        "Community forum",
+        "Hosting responsibly",
+        "Airbnb-friendly apartments",
+        "Join a free Hosting class",
+        "Find a co-host",
+      ],
+    },
+    {
+      title: "Airbnb",
+      items: [
+        "Newsroom",
+        "New features",
+        "Careers",
+        "Investors",
+        "Gift cards",
+        "Airbnb.org emergency stays",
+      ],
+    },
+  ];
+  const FOOTER_LINKS = ["Terms", "Sitemap", "Privacy", "Your Privacy Choices"];
+
   const _renderItemsFooter = () => {
     return (
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-8 pb-10 pl-22">
-        {/* Support Column */}
-        <div>
-          <h3 className="text-base font-semibold text-black-900 mb-4 pb-4">
-            Support
-          </h3>
-          <ul className="flex flex-col gap-y-4">
-            {[
-              "Help Center",
-              "AirCover",
-              "Anti-discrimination",
-              "Disability support",
-              "Cancellation options",
-              "Report neighborhood concern",
-            ].map((item) => (
-              <li key={item}>
-                <a
-                  href="#"
-                  className="text-base text-black-600 gap-y-16 hover:text-black-900 hover:underline"
-                >
-                  {item}
-                </a>
-              </li>
-            ))}
-          </ul>
-        </div>
-
-        {/* Hosting Column */}
-        <div>
-          <h3 className="text-base font-semibold text-black-900 mb-4 pb-4">
-            Hosting
-          </h3>
-          <ul className="flex flex-col gap-y-4">
-            {[
-              "Airbnb your home",
-              "AirCover for Hosts",
-              "Hosting resources",
-              "Community forum",
-              "Hosting responsibly",
-              "Airbnb-friendly apartments",
-              "Join a free Hosting class",
-              "Find a co-host",
-            ].map((item) => (
-              <li key={item}>
-                <a
-                  href="#"
-                  className="text-base text-black-600 hover:text-black-900 hover:underline"
-                >
-                  {item}
-                </a>
-              </li>
-            ))}
-          </ul>
-        </div>
-
-        {/* Airbnb Column */}
-        <div>
-          <h3 className="text-base font-semibold text-black-900 mb-4 pb-4">
-            Airbnb
-          </h3>
-          <ul className="flex flex-col gap-y-4">
-            {[
-              "Newsroom",
-              "New features",
-              "Careers",
-              "Investors",
-              "Gift cards",
-              "Airbnb.org emergency stays",
-            ].map((item) => (
-              <li key={item}>
-                <a
-                  href="#"
-                  className="text-base text-black-600 hover:text-black-900 hover:underline"
-                >
-                  {item}
-                </a>
-              </li>
-            ))}
-          </ul>
-        </div>
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-8 pb-10 md:flex md:justify-between lg:px-28">
+        {FOOTER_SECTIONS.map((section) => (
+          <div key={section.title}>
+            <h3 className="text-base font-semibold text-black-900 mb-4 pb-4">
+              {section.title}
+            </h3>
+            <ul className="flex flex-col gap-y-4">
+              {section.items.map((item) => (
+                <li key={item}>
+                  <a
+                    href="#"
+                    className="text-base text-black-600 hover:text-black-900 hover:underline"
+                  >
+                    {item}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </div>
+        ))}
       </div>
     );
   };
 
+  const _renderItemHotline = (url: string, src: string, alt: string) => (
+    <a href={url} className="text-gray-900">
+      <img
+        src={src}
+        alt={alt}
+        className="w-7 h-7 object-contain"
+      />
+    </a>
+  );
+
   const _renderBottomSection = () => {
     return (
-      <div className="mt-12 border-t border-gray-200 pt-8">
+      <div className="border-t border-gray-200 pt-8 md:px-20 bg-red-500">
         <div className="flex flex-col md:flex-row justify-between items-center">
           <div className="flex flex-wrap gap-2 justify-center md:justify-start mb-4 md:mb-0">
             <span className="text-base text-black-600">
               © 2025 Airbnb, Inc.
             </span>
             <span className="text-base text-black-600">·</span>
-            {["Terms", "Sitemap", "Privacy", "Your Privacy Choices"].map(
-              (item, index) => (
-                <React.Fragment key={item}>
-                  <a
-                    href="#"
-                    className="text-base text-black-600 hover:text-black-900 hover:underline cursor-pointer"
-                  >
-                    {item}
-                  </a>
-                  {index < 3 && (
-                    <span className="text-base text-black-600">·</span>
-                  )}
-                </React.Fragment>
-              )
-            )}
+            {FOOTER_LINKS.map((item, index) => (
+              <React.Fragment key={item}>
+                <a
+                  className="text-base text-black-600 hover:text-black-900 hover:underline cursor-pointer"
+                >
+                  {item}
+                </a>
+                {index < FOOTER_LINKS.length - 1 && <span className="text-base text-black-600">·</span>}
+              </React.Fragment>
+            ))}
           </div>
           <div className="flex items-center gap-x-8">
-            {/* Language Section */}
             <div className="flex items-center">
               <div className="flex gap-x-2 items-center p-1 hover:border-gray-300 hover:bg-gray-100 hover:rounded">
                 <span className="text-gray-900">🌐</span>
@@ -127,8 +107,6 @@ const Footer = () => {
                 </span>
               </div>
             </div>
-
-            {/* Currency Section */}
             <div className="flex items-center">
               <div className="flex gap-x-2 items-center p-1 hover:border-gray-300 hover:bg-gray-100 hover:rounded">
                 <span className="text-gray-900 font-semibold cursor-pointer">
@@ -139,30 +117,10 @@ const Footer = () => {
                 </span>
               </div>
             </div>
-
-            {/* Social Media Icons */}
             <div className="flex gap-x-2">
-              <a href="#" className="text-gray-900">
-                <img
-                  src={IMAGE_URL.FACEBOOK}
-                  alt="Facebook"
-                  className="w-7 h-7 object-contain"
-                />
-              </a>
-              <a href="#" className="text-gray-900">
-                <img
-                  src={IMAGE_URL.TWITTER}
-                  alt="Twitter"
-                  className="w-7 h-7 object-contain"
-                />
-              </a>
-              <a href="#" className="text-gray-900">
-                <img
-                  src={IMAGE_URL.INSTAGRAM}
-                  alt="Instagram"
-                  className="w-7 h-7 object-contain"
-                />
-              </a>
+              {_renderItemHotline('#', IMAGE_URL.FACEBOOK, 'Facebook')}
+              {_renderItemHotline('#', IMAGE_URL.TWITTER, 'LinkedIn')}
+              {_renderItemHotline('#', IMAGE_URL.INSTAGRAM, 'Instagram')}
             </div>
           </div>
         </div>
@@ -170,14 +128,10 @@ const Footer = () => {
     );
   };
 
-  // Component chính
   return (
     <footer className="bg-black-100 border-t border-gray-200">
-      <div className="mx-auto max-w-7xl px-4 py-8 sm:px-6 lg:px-8">
-        {/* Main Footer Content */}
+      <div className="md:!px-10 py-8 sm:px-6 lg:px-8 px-4">
         {_renderItemsFooter()}
-
-        {/* Bottom Section */}
         {_renderBottomSection()}
       </div>
     </footer>
