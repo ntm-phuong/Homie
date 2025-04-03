60077b6d38981f657d7939bea91575d324a82fd3
Chinh Khong <khongchinh757@gmail.com>
2025-04-03 15:59:07 +0700

    connect and get data mongoose

--- src/app/(pages)/home/page.tsx
+++ src/app/(pages)/home/page.tsx
@@ -1,11 +1,19 @@
 'use client'
 import ModalLogin from "@/src/components/modal-login/ModalLogin";
 import { Button } from "antd";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { IMAGE_URL } from "@/public";
 
 const Home = () => {
   const [isShowLogin, setIsShowLogin] = useState(false);
+  const [users, setUsers] = useState([]);
+  console.log(users, 'chinh13');
+  useEffect(() => {
+    fetch('/api/hello')
+      .then((res) => res.json())
+      .then((data) => setUsers(data))
+      .catch((error) => console.error('Lỗi khi lấy dữ liệu:', error));
+  }, []);
 
   return (
     <div className="bg-red-500">Đi đi em , do dự tới hạn mất
@@ -15,7 +23,7 @@ const Home = () => {
         }}
         className="bg-blue-500 text-white hover:bg-blue-700 focus:bg-blue-700 active:bg-blue-800"
       >
-        Click Login Modal
+        Click Login Modal dfvvfvv df
       </Button>
       {isShowLogin && (
         <ModalLogin isShowLogin={isShowLogin} setIsShowLogin={setIsShowLogin} />
