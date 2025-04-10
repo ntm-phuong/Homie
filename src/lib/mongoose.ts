3928514dac80c01502e282e955cb429ab9c96057
Chung1510 <chungtnguyenpt@gmail.com>
2025-04-10 12:59:12 +0700

    Mô tả nội dung thay đổi

--- src/lib/mongoose.ts
+++ src/lib/mongoose.ts
@@ -13,7 +13,7 @@ export async function connectDB() {
 
   if (!cached.promise) {
     cached.promise = mongoose.connect(MONGODB_URI, {
-      dbName: "homieIWS",
+      dbName: "homie",
     }).then((mongoose) => mongoose);
   }
 
