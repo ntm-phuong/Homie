48eca6fe91f73199257bf71d10a767217727aabb
Chinh Khong <khongchinh757@gmail.com>
2025-04-17 13:14:16 +0700

    ok

--- src/components/modal-register/ModalRegister.tsx
+++ src/components/modal-register/ModalRegister.tsx
@@ -1,8 +1,7 @@
-"use client";
-
-import React from "react";
+import React, { useState } from "react";
 import { Modal, Form, Input } from "antd";
 import { toast } from "react-toastify";
+import ModalVerify from "../modal-verify-otp/ModalVerify";
 
 interface ModalRegisterProps {
   isShowRegister: boolean;
@@ -15,6 +14,9 @@ const ModalRegister: React.FC<ModalRegisterProps> = ({
   setIsShowRegister,
   setIsShowLogin,
 }) => {
+  const [isShowVerify, setIsShowVerify] = useState(false); // state để quản lý modal OTP
+  const [email, setEmail] = useState(""); // state để lưu email khi đăng ký thành công
+
   const handleCancel = () => {
     setIsShowRegister(false);
   };
@@ -49,7 +51,8 @@ const ModalRegister: React.FC<ModalRegisterProps> = ({
       if (response.ok) {
         toast.success(data.message || "Registration successful!", { position: "top-right" });
         setIsShowRegister(false);
-        setIsShowLogin(true);
+        setEmail(email); 
+        setIsShowVerify(true); 
       } else {
         toast.error(data.message || "Registration failed!", { position: "top-right" });
       }
@@ -64,106 +67,114 @@ const ModalRegister: React.FC<ModalRegisterProps> = ({
   };
 
   return (
-    <Modal open={isShowRegister} onCancel={handleCancel} footer={null}>
-      <div className="text-center pb-4">
-        <h2 className="text-2xl font-bold">Register an account</h2>
-        <p className="text-gray-500 text-lg">
-          Create an account to start the experience
-        </p>
-      </div>
-      <Form
-        layout="vertical"
-        onFinish={handleRegister}
-        className="py-4 !px-2"
-        requiredMark="optional"
-      >
-        <Form.Item
-          label={
-            <span className="font-bold text-lg">
-              Email <span className="text-red-500">*</span>
-            </span>
-          }
-          name="email"
-          rules={[{ required: true, message: "Please enter your email!" }]}
-        >
-          <Input placeholder="Enter your email" className="h-[50px] text-md" />
-        </Form.Item>
-
-        <Form.Item
-          label={
-            <span className="font-bold text-lg">
-              Password <span className="text-red-500">*</span>
-            </span>
-          }
-          name="password"
-          rules={[{ required: true, message: "Please enter the password!" }]}
-        >
-          <Input.Password
-            placeholder="Enter your password"
-            className="h-[50px] text-md"
-          />
-        </Form.Item>
-
-        <Form.Item
-          label={
-            <span className="font-bold text-lg">
-              Confirm Password <span className="text-red-500">*</span>
-            </span>
-          }
-          name="confirmPassword"
-          dependencies={["password"]}
-          rules={[
-            { required: true, message: "Please confirm the password!" },
-            ({ getFieldValue }) => ({
-              validator(_, value) {
-                if (!value || getFieldValue("password") === value) {
-                  return Promise.resolve();
-                }
-                return Promise.reject(new Error("Password does not match!"));
-              },
-            }),
-          ]}
-        >
-          <Input.Password
-            placeholder="Re-enter your password"
-            className="h-[50px] text-md"
-          />
-        </Form.Item>
-
-        <Form.Item
-          name="agreement"
-          valuePropName="checked"
-          rules={[
-            {
-              validator: (_, value) =>
-                value
-                  ? Promise.resolve()
-                  : Promise.reject(
-                    new Error("You need to agree with the terms!")
-                  ),
-            },
-          ]}
-        >
-          <div className="flex items-center gap-2">
-            <input type="checkbox" /> <p>I agree to the terms and conditions</p>
-          </div>
-        </Form.Item>
-        <Form.Item>
-          <button className="!text-white !h-[50px] !w-full !text-xl !font-medium bg-main !hover:none rounded-xl text-center cursor-pointer">
-            Register
-          </button>
-        </Form.Item>
-        <div className="text-center text-[16px]">
-          Already have an account?{" "}
-          <span
-            onClick={switchToLogin}
-            className="font-semibold cursor-pointer"
-          >
-            Sign In
-          </span>
+    <>
+      <Modal open={isShowRegister} onCancel={handleCancel} footer={null}>
+        <div className="text-center pb-4">
+          <h2 className="text-2xl font-bold">Register an account</h2>
+          <p className="text-gray-500 text-lg">
+            Create an account to start the experience
+          </p>
         </div>
-      </Form>
-    </Modal>
+        <Form
+          layout="vertical"
+          onFinish={handleRegister}
+          className="py-4 !px-2"
+          requiredMark="optional"
+        >
+          <Form.Item
+            label={
+              <span className="font-bold text-lg">
+                Email <span className="text-red-500">*</span>
+              </span>
+            }
+            name="email"
+            rules={[{ required: true, message: "Please enter your email!" }]}
+          >
+            <Input placeholder="Enter your email" className="h-[50px] text-md" />
+          </Form.Item>
+
+          <Form.Item
+            label={
+              <span className="font-bold text-lg">
+                Password <span className="text-red-500">*</span>
+              </span>
+            }
+            name="password"
+            rules={[{ required: true, message: "Please enter the password!" }]}
+          >
+            <Input.Password
+              placeholder="Enter your password"
+              className="h-[50px] text-md"
+            />
+          </Form.Item>
+
+          <Form.Item
+            label={
+              <span className="font-bold text-lg">
+                Confirm Password <span className="text-red-500">*</span>
+              </span>
+            }
+            name="confirmPassword"
+            dependencies={["password"]}
+            rules={[
+              { required: true, message: "Please confirm the password!" },
+              ({ getFieldValue }) => ({
+                validator(_, value) {
+                  if (!value || getFieldValue("password") === value) {
+                    return Promise.resolve();
+                  }
+                  return Promise.reject(new Error("Password does not match!"));
+                },
+              }),
+            ]}
+          >
+            <Input.Password
+              placeholder="Re-enter your password"
+              className="h-[50px] text-md"
+            />
+          </Form.Item>
+
+          <Form.Item
+            name="agreement"
+            valuePropName="checked"
+            rules={[
+              {
+                validator: (_, value) =>
+                  value
+                    ? Promise.resolve()
+                    : Promise.reject(
+                        new Error("You need to agree with the terms!")
+                      ),
+              },
+            ]}
+          >
+            <div className="flex items-center gap-2">
+              <input type="checkbox" /> <p>I agree to the terms and conditions</p>
+            </div>
+          </Form.Item>
+          <Form.Item>
+            <button className="!text-white !h-[50px] !w-full !text-xl !font-medium bg-main !hover:none rounded-xl text-center cursor-pointer">
+              Register
+            </button>
+          </Form.Item>
+          <div className="text-center text-[16px]">
+            Already have an account?{" "}
+            <span
+              onClick={switchToLogin}
+              className="font-semibold cursor-pointer"
+            >
+              Sign In
+            </span>
+          </div>
+        </Form>
+      </Modal>
+
+      <ModalVerify
+        isShowVerify={isShowVerify}
+        setIsShowVerify={setIsShowVerify}
+        email={email}
+      />
+    </>
   );
 };
 
